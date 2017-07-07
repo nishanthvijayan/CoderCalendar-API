992dda7e0c290e5057ee1145e6c4460e728db4bd
Nishanth Vijayan <nishanththegr8@gmail.com>
2017-07-07 17:17:05 +0530

    Use simple key value store as in memory cache

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 var http = require("http");
 var aggregate = require("./aggregate");
+var cache = require('memory-cache');
+
+var cacheTTL = 15 * 60 * 1000;
 
 var server = http.createServer(function(req, res){
-    aggregate().then(function(contests){
-        res.writeHead(200, {"Content-Type": "application/json"});
+    var cachedResults = cache.get('results');
+    if(cachedResults){
         res.write(JSON.stringify(
-        	{"results": contests}
+            {"results": cachedResults}
         ));
         res.end();
-    })
-    .catch(function(error){
-    	res.writeHead(500);
-    	res.end;
-    });
+    }else{
+        console.log("Cache miss!");
+        aggregate().then(function(contests){
+            cache.put('results', contests, cacheTTL);
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.write(JSON.stringify(
+                {"results": contests}
+            ));
+            res.end();
+        })
+        .catch(function(error){
+            res.writeHead(500);
+            res.end;
+        });
+    }
 });
 
 server.listen(8000);
