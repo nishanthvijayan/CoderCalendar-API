c3eecbfd7dbc73f97a87410e4c2bcec4baae760c
Nishanth Vijayan <nishanththegr8@gmail.com>
2017-07-08 19:45:06 +0530

    Refactor: Extract to function Codechef contest row parsing

--- parsers/codechef.js
+++ parsers/codechef.js
@@ -1,6 +1,20 @@
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+function parseContestDetails($, contest_row){
+    var details = $(contest_row).find("td");
+    var start_time = new Date(details.eq(2).text()).getTime()/1000;
+    var end_time = new Date(details.eq(3).text()).getTime()/1000;
+    return {
+            "name": details.eq(1).text(),
+            "url": "http://www.codechef.com" + details.eq(1).find('a').attr("href"),
+            "platform": 'codechef',
+            "start_time": start_time,
+            "end_time": end_time,
+            "duration": (end_time - start_time),
+        }
+}
+
 var codechef = function(){
     return axios.get("http://www.codechef.com/contests", {timeout: 30000})
                 .then(function(response){
@@ -13,34 +27,12 @@ var codechef = function(){
                             contest_tables[headings.eq(i).text()] = statusdiv.eq(i).find("tr").slice(1);
                         }
                     }
-                    var contests = contest_tables['Present Contests'].map(function(i, elem){
-                        var present_contest = $(elem);
-                        var details = present_contest.find("td");
-                        var start_time = new Date(details.eq(2).text()).getTime()/1000;
-                        var end_time = new Date(details.eq(3).text()).getTime()/1000;
-                        return {
-                                "name": details.eq(1).text(),
-                                "url": "http://www.codechef.com" + details.eq(1).find('a').attr("href"),
-                                "platform": 'codechef',
-                                "start_time": start_time,
-                                "end_time": end_time,
-                                "duration": (end_time - start_time),
-                            }
+                    var contests = contest_tables['Present Contests'].map((i, elem)=>{
+                        return parseContestDetails($, elem);
                     }).get();
 
-                    contests = contests.concat(contest_tables["Future Contests"].map(function(i, elem){
-                        var upcoming_contest = $(elem);
-                        var details = upcoming_contest.find("td")
-                        var start_time = new Date(details.eq(2).text()).getTime()/1000;
-                        var end_time = new Date(details.eq(3).text()).getTime()/1000;
-                        return {
-                                "name": details.eq(1).text(),
-                                "url": "http://www.codechef.com" + details.eq(1).find('a').attr("href"),
-                                "platform": 'codechef',
-                                "start_time": start_time,
-                                "end_time": end_time,
-                                "duration": (end_time - start_time),
-                            }
+                    contests = contests.concat(contest_tables["Future Contests"].map((i, elem)=>{
+                        return parseContestDetails($, elem);
                     }).get());
 
                     return contests;
