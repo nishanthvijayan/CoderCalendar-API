71142c5bc1d58109e188645bcef748eb9c86c2e9
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-08-19 23:50:07 +0530

    Fix csacademy parser

--- src/parsers/csacademy.js
+++ src/parsers/csacademy.js
@@ -11,7 +11,7 @@ const csacademy = () => {
     timeout: 15000,
   };
   return axios.get('https://csacademy.com/contests', options)
-    .then(response => response.data.state.contest
+    .then(response => response.data.state.Contest
       .filter(contest => contest.startTime != null).map(contest => ({
         name: contest.longName,
         url: `https://csacademy.com/contest/${contest.name}`,
