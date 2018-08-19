edcc56a707757e86fff095ef71a5d0f084909ffb
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-08-19 17:31:08 +0530

    Refactor

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const url = require('url');
 const aggregate = require('./aggregate');
 const filter = require('./filter');
 
-const cacheTTL = 15 * 60 * 1000;
+const CACHE_TTL = 15 * 60 * 1000;
+
+const respondWithResult = (res, results) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify({ results }));
+  res.end();
+};
+
+const respondWithError = (res, err) => {
+  res.writeHead(500, err.toString());
+  res.end();
+};
 
 const server = http.createServer((req, res) => {
   const urlParts = url.parse(req.url, true);
@@ -15,33 +26,23 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-  const options = {
+  const filterOpts = {
     platform: urlParts.query.platform || null,
     status: urlParts.query.status || null,
   };
 
   const cachedResults = cache.get('results');
   if (cachedResults) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(
-      { results: filter(cachedResults, options) },
-    ));
-    res.end();
-  } else {
-    aggregate().then((contests) => {
-      cache.put('results', contests, cacheTTL);
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify(
-        { results: filter(contests, options) },
-      ));
-      res.end();
-    })
-      .catch((error) => {
-        console.log(error.toString());
-        res.writeHead(500);
-        res.end();
-      });
+    respondWithResult(res, filter(cachedResults, filterOpts));
+    return;
   }
+
+  aggregate()
+    .then((results) => {
+      cache.put('results', results, CACHE_TTL);
+      respondWithResult(res, filter(results, filterOpts));
+    })
+    .catch(error => respondWithError(res, error));
 });
 
 server.listen(8000);
