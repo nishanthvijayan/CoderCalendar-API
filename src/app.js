39fe4d6d5701eb85ff321c02c4c805a153d89cfc
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-10-18 16:13:05 +0530

    Fix cron pattern

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const server = require('./server');
 const runner = require('./runner');
 
 // Set up job that'll run runner every 10 minutes
-new CronJob('* */10 * * * *', runner).start();
+new CronJob('*/10 * * * *', runner).start();
 
 // Run runner once now
 runner();
