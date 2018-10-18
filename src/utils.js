da3ebe70488742990d2488cc609e7873c9faa4cf
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-10-18 16:12:56 +0530

    Add date to parser failure logger

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const parserErrorHandler = platform => error => console.log(platform, error.toString());
+const parserErrorHandler = platform => error => console.log(new Date(), platform, error.toString());
 
 const flat = arr => arr.reduce((res, it) => res.concat(Array.isArray(it) ? flat(it) : it), []);
 
