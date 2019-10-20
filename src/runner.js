3e0ade2821f2b2262eb1aea880b58108bd5a45c8
Gaurav Mohla <gm019528@gmail.com>
2019-10-20 13:56:34 +0530

    reverting the test changes

--- src/runner.js
+++ src/runner.js
@@ -29,20 +29,18 @@ const runner = () => axios.all([
 ])
   .then((contestsByPlatform) => {
     const contests = flat(contestsByPlatform.filter(it => Array.isArray(it)));
-    
+
     const curTime = getCurrentTimeInSeconds();
 
     const sortByStartTime = (a, b) => a.startTime - b.startTime;
     const sortByEndTime = (a, b) => a.endTime - b.endTime;
-    
-    
+
     const isOngoing = contest => contest.startTime < curTime && contest.endTime > curTime;
     const isUpcoming = contest => contest.startTime > curTime && contest.endTime > curTime;
-    const v = contest => contest.startTime ;
-    
+
     const ongoingContests = contests.filter(isOngoing).sort(sortByEndTime);
-    const upcomingContests = contests.filter(v).sort(sortByStartTime);
-    
+    const upcomingContests = contests.filter(isUpcoming).sort(sortByStartTime);
+
     cache.put('results', {
       timestamp: curTime,
       ongoing: ongoingContests,
