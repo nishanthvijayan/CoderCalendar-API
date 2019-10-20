b35122345551b14fd6f1f441c0108de7b2aa95e0
Gaurav Mohla <gm019528@gmail.com>
2019-10-20 13:44:56 +0530

    add codingninjas capability

--- src/runner.js
+++ src/runner.js
@@ -12,6 +12,7 @@ const atcoder = require('./parsers/atcoder');
 const csacademy = require('./parsers/csacademy');
 const coj = require('./parsers/coj');
 const kaggle = require('./parsers/kaggle');
+const codingninjas = require('./parsers/codingninjas');
 
 const runner = () => axios.all([
   codeforces(),
@@ -24,21 +25,24 @@ const runner = () => axios.all([
   csacademy(),
   coj(),
   kaggle(),
+  codingninjas(),
 ])
   .then((contestsByPlatform) => {
     const contests = flat(contestsByPlatform.filter(it => Array.isArray(it)));
-
+    
     const curTime = getCurrentTimeInSeconds();
 
     const sortByStartTime = (a, b) => a.startTime - b.startTime;
     const sortByEndTime = (a, b) => a.endTime - b.endTime;
-
+    
+    
     const isOngoing = contest => contest.startTime < curTime && contest.endTime > curTime;
     const isUpcoming = contest => contest.startTime > curTime && contest.endTime > curTime;
-
+    const v = contest => contest.startTime ;
+    
     const ongoingContests = contests.filter(isOngoing).sort(sortByEndTime);
-    const upcomingContests = contests.filter(isUpcoming).sort(sortByStartTime);
-
+    const upcomingContests = contests.filter(v).sort(sortByStartTime);
+    
     cache.put('results', {
       timestamp: curTime,
       ongoing: ongoingContests,
